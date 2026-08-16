f6aca65c6cf04966b51bf6ac9248fe380b9ed890
RawdaHossam <rawda5204@gmail.com>
2026-08-16 22:54:06 +0300

    Add files via upload

---
+++ Kawadir Travel.js
@@ -0,0 +1,64 @@
+const navMenu = document.getElementById('nav--menu'),
+      navToggle = document.getElementById('nav--toggle'),
+      navClose = document.getElementById('nav--close')
+
+// menu show
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show--menu')
+  })
+}
+
+// nav hidden
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show--menu')
+  })
+}
+
+// remove menu mobile
+const navLink = document.querySelectorAll('.nav--link')
+
+const linkAction = () => {
+  navMenu.classList.remove('show--menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// JS
+const themeBtn = document.getElementById("theme-button");
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-theme");
+  document.body.classList.toggle("dark-theme");
+});
+
+// animation
+
+/* ===== Scroll Reveal Animation ===== */
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const revealTop = el.getBoundingClientRect().top;
+    const revealPoint = 100; // trigger point
+
+    if (revealTop < windowHeight - revealPoint) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+
+
+
+
+
+
+
+
+
+
+
